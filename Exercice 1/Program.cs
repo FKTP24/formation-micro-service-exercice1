3853242f484d055bd108414037ad6e0f9a83ba15
Frank Thermeau <fkt@protection24.com>
2020-12-01 17:12:20 +0100

    Test modifications

--- Exercice 1/Program.cs
+++ Exercice 1/Program.cs
@@ -6,7 +6,7 @@ namespace Exercice_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Test !");
         }
     }
 }
